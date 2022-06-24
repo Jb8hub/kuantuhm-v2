c7a4c4675a71e27a63a0b419662ac69b70053210
Jb8hub <jb8git.hub@gmail.com>
2022-06-24 17:42:36 -0300

    Aurebash font

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,46 +1,49 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable react/button-has-type */
 /* eslint-disable react/function-component-definition */
 import React, { useState } from "react";
-import { TiTimesOutline, TiThMenuOutline } from "react-icons/ti";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import Logo from "../assets/logo.png";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
+
   const handleNav = () => {
     setNav(!nav);
   };
 
   return (
-    <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-[#fdfeff]">
-      <h1 className="w-full text-3xl font-bold text-[#00df9a]">KUANTUHM*</h1>
-      <ul className="flex hidden">
-        <li className="p-4"> Home </li>
-        <li className="p-4"> Company </li>
-        <li className="p-4"> Resources </li>
-        <li className="p-4"> About </li>
-        <li className="p-4"> Contact </li>
+    <div className="flex justify-between items-center h-16 max-w-[1240px] mx-auto px-4 text-white">
+      <img className="w-[40%]" src={Logo} alt="/" />
+      <ul className="hidden md:flex">
+        <li className="p-4">Home</li>
+        <li className="p-4">Company</li>
+        <li className="p-4">Resources</li>
+        <li className="p-4">About</li>
+        <li className="p-4">Contact</li>
       </ul>
-      <div onClick={handleNav}>
-        {!nav ? <TiTimesOutline size={48} /> : <TiThMenuOutline size={36} />}
+      <div onClick={handleNav} className="block md:hidden">
+        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
-      <div
+      <ul
         className={
-          !nav
-            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-[#fdfeff] bg-[#1f1f1f] ease-in-out duration-500"
-            : "fixed left-[-100%]"
+          nav
+            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
+            : "ease-in-out duration-500 fixed left-[-100%]"
         }
       >
-        <h1 className="w-full text-3xl font-bold text-[#00df9a]">KUANTUHM*</h1>
-        <ul className="pt-24 uppercase">
-          <li className="p-4 border-b border-[#fdfeff]">Home</li>
-          <li className="p-4 border-b border-[#fdfeff]">Company</li>
-          <li className="p-4 border-b border-[#fdfeff]"> Resources </li>
-          <li className="p-4 border-b border-[#fdfeff]"> About </li>
-          <li className="p-4 border-b border-[#fdfeff]"> Contact </li>
-        </ul>
-      </div>
+        <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">
+          KUANTUHM*
+        </h1>
+        <li className="p-4 border-b border-gray-600">Home</li>
+        <li className="p-4 border-b border-gray-600">Company</li>
+        <li className="p-4 border-b border-gray-600">Resources</li>
+        <li className="p-4 border-b border-gray-600">About</li>
+        <li className="p-4">Contact</li>
+      </ul>
     </div>
   );
 };
+
 export default Navbar;
